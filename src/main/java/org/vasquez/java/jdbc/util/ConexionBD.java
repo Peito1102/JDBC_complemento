674f51f7e0feafe125b2195a62deeb369a0bf96c
Peito1102 <renzitiwin@gmail.com>
2024-09-20 22:37:00 -0500

    Complemento de JDBC(34)

---
+++ src/main/java/org/vasquez/java/jdbc/util/ConexionBD.java
@@ -0,0 +1,15 @@
+package org.vasquez.java.jdbc.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConexionBD {
+    private static String url = "jdbc:mysql://localhost:3306/java_curso?serverTimezone=UTC";
+    private static String username = "root";
+    private static String password = "mysql";
+    private static Connection connection;
+    public static Connection getInstance() throws SQLException {
+            return connection = DriverManager.getConnection(url,username,password);
+    }
+}
